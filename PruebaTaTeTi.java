ac43d3902f7bb582d97237cfddceb722737ea6f3
jsara <jsaraviaa98@gmail.com>
2022-09-03 17:44:14 -0300

    .

---
+++ PruebaTaTeTi.java
@@ -0,0 +1,13 @@
+
+
+public class PruebaTaTeTi {
+    
+    public static void main(String[] args) {
+    
+    TaTeTi jugar= new TaTeTi();
+    jugar.crearYllenarTablero();
+    jugar.mostrarTablero();
+    jugar.jugar();
+    }
+    
+    }
