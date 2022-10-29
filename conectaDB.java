6b27618ce7638d2fbd0af0ef7c3b067395de172d
Jazmin Saravia <jsaraviaa98@gmail.com>
2022-10-29 09:14:51 -0300

    added conector

--- conectaDB.java
+++ conectaDB.java
@@ -1,6 +1,9 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
+
+import com.mysql.cj.xdevapi.PreparableStatement;
+
 import java.sql.ResultSet;
 
 
@@ -10,8 +13,12 @@ public class conectaDB {
             //Crear conexion
             Connection miConexion= DriverManager.getConnection("jdbc:mysql://localhost:3306/tateti","root","");
             Statement miStatement= miConexion.createStatement();
+            //obtener registros
             ResultSet miResultSet = miStatement.executeQuery("SELECT * FROM idioma" );
-        
+            //update registros
+            //miStatement.exceuteUpdate(instruccionSQL);
+            
+         
 
             while(miResultSet.next()){
                 System.out.println(miResultSet.getString("idIdioma")+" "+ miResultSet.getString("descripcion"));
