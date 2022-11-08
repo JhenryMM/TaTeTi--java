79cb8c48e475f171aedbde1e0536f1150a9e5ace
JhenryMM <65234337+JhenryMM@users.noreply.github.com>
2022-11-08 00:20:06 -0300

    Add files via upload

--- PruebaTaTeTi.java
+++ PruebaTaTeTi.java
@@ -1,26 +1,22 @@
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
 import java.util.Scanner;
 import java.util.Date;
 public class PruebaTaTeTi {
     
     public static void main(String[] args) {
-    conectaDB d = new conectaDB("localhost:3306/tateti","root","");
-    System.out.println("1.jugar\n2.Cambiar idomas\n3.ver estadisticas\n4.salir");
+
+    conectaDB d = new conectaDB("localhost:3306/tateti","root","admin1");
+    System.out.println("1.jugar\n2.Cambiar idioma\n3.ver estadisticas\n4.salir");
     Scanner l = new Scanner(System.in);
     int op1= l.nextInt();
-    Date date=new Date();
-    DateFormat hourdateFormat= new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-    LocalDateTime datetime = LocalDateTime.now();
-    
     menu(op1,d,l);
-
-
+    //faltaria agregar idioma
     }
 
     public static void menu(int op1, conectaDB d,Scanner l) {
         String gano="perdio";
+
         switch(op1){
             case 1: 
             Date inicio= new Date();
--- conectaDB.java
+++ conectaDB.java
@@ -1,14 +1,16 @@
+import java.sql.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
 import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
 import java.sql.ResultSet;
-import java.time.LocalDateTime;
+
+
 public class conectaDB {
     private String  conexion;
+
     private String  usuario;
     private String  password;
     public conectaDB(String conexion, String usuario, String password){
@@ -20,21 +22,16 @@ public class conectaDB {
     public void imprimirTablaIdiomas(){
         System.out.println("Seleccione su idioma");
         try {
-            //Crear conexion
-            Connection miConexion= DriverManager.getConnection("jdbc:mysql://"+this.conexion,this.usuario,this.password);
-            Statement miStatement= miConexion.createStatement();
-            //obtener registros
-            ResultSet miResultSet = miStatement.executeQuery("SELECT * FROM idioma" );
-            //update registros
-            //miStatement.exceuteUpdate(instruccionSQL);
-            
-         
-
-            while(miResultSet.next()){
-                System.out.println(miResultSet.getString("idIdioma")+" "+ miResultSet.getString("descripcion"));
-            }
-
-
+////            //Crear conexion
+////            Connection miConexion= DriverManager.getConnection("jdbc:mysql://localhost:3306"+this.conexion,this.usuario,this.password);
+////            Statement miStatement= miConexion.createStatement();
+////            //obtener registros
+////            ResultSet miResultSet = miStatement.executeQuery("SELECT * FROM idioma" );
+////            //update registros
+////            //miStatement.exceuteUpdate(instruccionSQL);
+//                while(miResultSet.next()){
+//                    System.out.println(miResultSet.getString("idIdioma")+" "+ miResultSet.getString("descripcion"));
+//                }
         }catch(Exception e){
             System.out.println(e.getMessage());
         }
@@ -43,23 +40,18 @@ public class conectaDB {
     public void imprimirEstadisticas(){
         System.out.println("Estas son tus Estadisticas");
         try {
-            //Crear conexion
-            Connection miConexion= DriverManager.getConnection("jdbc:mysql://"+this.conexion,this.usuario,this.password);
-            Statement miStatement= miConexion.createStatement();
-            //obtener registros
-            ResultSet miResultSet = miStatement.executeQuery("SELECT * FROM registrodepartida" );
-            //update registros
-            //miStatement.exceuteUpdate(instruccionSQL);
-           
-           
-            
 
-
-            while(miResultSet.next()){
-               
-                System.out.println("La partida inicio "+miResultSet.getString("inicioDePartida")+ " y finalizo el "+miResultSet.getString("FinDePartida")+" y el jugador "+ miResultSet.getString("nombreJugador")+" "+miResultSet.getString("Gano")+" contra la maquina");
-            }
-
+//            //Crear conexion
+//            Connection miConexion= DriverManager.getConnection("jdbc:mysql://"+this.conexion,this.usuario,this.password);
+//            Statement miStatement= miConexion.createStatement();
+//            //obtener registros
+//            ResultSet miResultSet = miStatement.executeQuery("SELECT * FROM registrodepartida" );
+//            //update registros
+//            //miStatement.exceuteUpdate(instruccionSQL);
+//            while(miResultSet.next()){
+//
+//                System.out.println("La partida inicio "+miResultSet.getString("inicioDePartida")+ " y finalizo el "+miResultSet.getString("FinDePartida")+" y el jugador "+ miResultSet.getString("nombreJugador")+" "+miResultSet.getString("Gano")+" contra la maquina");
+//            }
 
         }catch(Exception e){
             System.out.println(e.getMessage());
@@ -76,15 +68,21 @@ public class conectaDB {
             //Crear conexion
             Connection miConexion= DriverManager.getConnection("jdbc:mysql://"+this.conexion,this.usuario,this.password);
             Statement miStatement= miConexion.createStatement();
-           
-            //miStatement.exceuteUpdate(instruccionSQL);
-            String instruccionSQL= "INSERT INTO registrodepartida(inicioDePartida,FinDePartida,gano,nombreJugador) VALUES ('"+date.format(inicio)+"','"+date.format(fin)+"','"+ganador+"','"+nombre+"')";
+            String instruccionSQL= "INSERT INTO jugador(nombre) VALUES ('"+nombre+"')";
             miStatement.executeUpdate(instruccionSQL);
-            
-       
+            PreparedStatement ps =miConexion.prepareStatement("SELECT*FROM jugador WHERE nombre = '"+nombre+"'");
+            ResultSet jugador =ps.executeQuery();
+            int idjugador = 0;
 
+            while(jugador.next()){
+                idjugador=jugador.getInt("idjugador");
+            }
+
+            int dato=idjugador;
+            String instruccionSQLB = "INSERT INTO registrodepartida(inicioDePartida,FinDePartida,gano,idjugador) VALUES ('"+
+                    date.format(inicio)+"','"+date.format(fin)+"','"+ganador+"','"+dato+"')";
+            miStatement.executeUpdate(instruccionSQLB);
             System.out.println("Se registro correctamente");
-
 
         }catch(Exception e){
             System.out.println(e.getMessage());
